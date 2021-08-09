36ea0dac3718843e9988db153188b21cbf55d3ad
LanceHE <xyzlancehe@gmail.com>
2021-08-09 17:31:53 +0800

    编写服务器中

--- 斗地主C++/Game/onlinegame.cpp
+++ 斗地主C++/Game/onlinegame.cpp
@@ -1,1 +1,91 @@
 ﻿#include "onlinegame.h"
+#include <thread>
+
+namespace PokerGame
+{
+	namespace FAL
+	{
+
+		void OnlineServer::Startup()
+		{
+			auto broadcastSceneThread = std::thread(&OnlineServer::BroadCastSceneThread, this);
+			broadcastSceneThread.detach();
+		}
+
+		void OnlineServer::Reset()
+		{
+			this->resetFlag = 0b1;
+			while (this->resetFlag)
+			{
+
+			}
+		}
+
+		void OnlineServer::Init()
+		{
+			this->broadcastSocket_fd = socket(AF_INET, SOCK_DGRAM, 0);
+			if (this->broadcastSocket_fd == INVALID_SOCKET)
+			{
+				throw ServerInitFailedException();
+			}
+			this->clientBroadCastAddr.sin_family = AF_INET;
+			this->clientBroadCastAddr.sin_port = this->port;
+			in_addr broadcastAddr;
+			inet_pton(AF_INET, "239.0.1.10", &broadcastAddr.s_addr);
+			inet_pton(AF_INET, "239.0.1.10", &this->clientBroadCastAddr.sin_addr.s_addr);
+			int setMultiCastResult = setsockopt(this->broadcastSocket_fd, IPPROTO_IP, IP_MULTICAST_IF, reinterpret_cast<char*>(&broadcastAddr), sizeof(in_addr));
+			if (setMultiCastResult != 0)
+			{
+				throw ServerInitFailedException();
+			}
+		}
+
+		OnlineServer::OnlineServer()
+		{
+			this->port = 6666;
+			
+		}
+
+		OnlineServer::OnlineServer(int port)
+		{
+			throw std::exception("not implemented yet");
+		}
+
+		void OnlineServer::BroadCastSceneThread()
+		{
+			while (true)
+			{
+				int reset = this->resetFlag & 0b1;
+				if (reset)
+				{
+					this->resetFlag &= 0b0;
+					return;
+				}
+
+				Scene current = this->FormCurrentScene();
+				int sentlen = sendto(this->broadcastSocket_fd,
+					reinterpret_cast<char*>(&current),
+					sizeof(Scene),
+					0,
+					reinterpret_cast<sockaddr*>(&this->clientBroadCastAddr),
+					sizeof(sockaddr_in));
+				using namespace std::chrono_literals;
+				if (sentlen == -1)
+				{
+
+				}
+				std::this_thread::sleep_for(0.5s);
+			}
+		}
+
+		Scene OnlineServer::FormCurrentScene() noexcept
+		{
+			Scene currentScene;
+			currentScene.CurrentStage = static_cast<char>(this->gameStage);
+			
+
+
+			return currentScene;
+		}
+	}
+}
--- 斗地主C++/Game/onlinegame.h
+++ 斗地主C++/Game/onlinegame.h
@@ -1,6 +1,79 @@
 ﻿#pragma once
-
+#include <string>
+#include <memory>
 #include <WinSock2.h>
+#include <ws2tcpip.h> 
+#include "poker.h"
+#include "cardtype.h"
+
+namespace PokerGame
+{
+	namespace FAL
+	{
+		constexpr auto STAGE_PREPARING = ((char)0);
+		constexpr auto STAGE_DETERMINE_LANDLORD = ((char)1);
+		constexpr auto STAGE_MAINLOOP_ONGOING = ((char)2);
+		constexpr auto STAGE_END = ((char)3);
+		constexpr auto ACTIVE_TYPE_ACTIVE = ((char)0);
+		constexpr auto ACTIVE_TYPE_FOLLOW = ((char)1);
+		constexpr auto ACTIVE_TYPE_PASS = ((char)2);
+
+		struct Scene
+		{
+			char CurrentStage;
+			char LandlordWillingness[3];
+			char LandlordIndex;
+			char ActiveIndex;
+			char ActiveType;
+			char CardLeftCount[3];
+			char LandlordCards[3];
+			char LastActType;
+			char SecondLastActType;
+			char LastCardDrop[20];
+			char SecondLastCardDrop[20];
+		};
+
+		int scene_struct_len = sizeof(Scene);
+
+		struct ClientID
+		{
+			int hashCode;
+			sockaddr_in ip;
+		};
+
+		class OnlineServer
+		{
+		public:
+			void Startup();
+			void Reset();
+			void Init();
+			OnlineServer();
+			OnlineServer(int port);
+		private:
+			void BroadCastSceneThread();
+			void ListenThread();
+			int port;
+			volatile int resetFlag;
+			sockaddr_in clientBroadCastAddr;
+			SOCKET broadcastSocket_fd;
+			Scene FormCurrentScene() noexcept;
+			ClientID playerIDs[3];
+			int gameStage;
+			int isLandlordDetermined;
+			int landlordIndex;
+			int nextActPlayerIndex;
+			char nextActType;
+			std::unique_ptr<PokerCardCollection> lastAct;
+			std::unique_ptr<PokerCardCollection> secondLastAct;
+			std::unique_ptr<PokerCardCollection> playerCards[3];
+
+		};
+
+		class ServerInitFailedException : public std::exception
+		{
+
+		};
 
 
-
+	}
+}
